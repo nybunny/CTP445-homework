eb6c64a35ce702ec69f1a69435dddee21475f57e
unknown <aleron@kaist.ac.kr>
2020-12-22 22:26:07 +0900

    hw02

---
+++ PHW2/Assets/PHW2/PHW2_main.cs
@@ -0,0 +1,122 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+
+namespace UnityEngine.XR.ARFoundation.Samples
+{
+    /// <summary>
+    /// Moves the ARSessionOrigin in such a way that it makes the given content appear to be
+    /// at a given location acquired via a raycast.
+    /// </summary>
+    [RequireComponent(typeof(ARSessionOrigin))]
+    [RequireComponent(typeof(ARRaycastManager))]
+    public class PHW2_main : MonoBehaviour
+    {
+        [SerializeField]
+        [Tooltip("A transform which should be made to appear to be at the touch point.")]
+        Transform m_Content;
+
+        /// <summary>
+        /// A transform which should be made to appear to be at the touch point.
+        /// </summary>
+        public Transform content
+        {
+            get { return m_Content; }
+            set { m_Content = value; }
+        }
+
+        [SerializeField]
+        [Tooltip("The rotation the content should appear to have.")]
+        Quaternion m_Rotation;
+        bool flag_smoothing;
+        /// Add required variables
+        float m_Scale;
+        Vector3 newScale;
+        float moveSpeed = 5f;
+    
+        public Quaternion rotation
+        {
+            get { return m_Rotation; }
+            set
+            {
+                m_Rotation = value;
+                if (m_SessionOrigin != null)
+                    m_SessionOrigin.MakeContentAppearAt(content, content.transform.position, m_Rotation);
+            }
+        }
+
+        //////////////////////////// TO DO #1 ///////////////////////////
+        public float scale
+        {
+           get { return m_Scale; }
+           set
+            {
+                m_Scale = value;
+                if (m_SessionOrigin != null)
+                {
+                    newScale.x = m_Scale; newScale.y = m_Scale; newScale.z = m_Scale;
+                    content.transform.localScale = newScale;
+                    m_SessionOrigin.MakeContentAppearAt(content, content.transform.position);
+                }
+                //Debug.Log("scale=" + m_Scale);
+            }
+        }
+        /////////////////////////////////////////////////////////////////
+
+
+        //////////////////////////// TO DO #2 ///////////////////////////
+        /// Implement function to toggle boolean variable 'flag_smoothing'
+        
+        public bool toggle_flag
+        {
+            get { return flag_smoothing; }
+            set
+            {
+                flag_smoothing = value;
+                Debug.Log("dkdkdkdkd");
+                Debug.Log(flag_smoothing);
+            }
+        }
+
+
+        /////////////////////////////////////////////////////////////////
+
+        void Awake()
+        {
+            m_SessionOrigin = GetComponent<ARSessionOrigin>();
+            m_RaycastManager = GetComponent<ARRaycastManager>();
+            flag_smoothing = false;
+        }
+
+        void Update()
+        {
+            if (Input.touchCount == 0 || m_Content == null)
+                return;
+
+            var touch = Input.GetTouch(0);
+
+            if (m_RaycastManager.Raycast(touch.position, s_Hits, TrackableType.PlaneWithinPolygon))
+            {
+                var hitPose = s_Hits[0].pose;
+
+                //////////////////////////// TO DO #2 ///////////////////////////
+                if (flag_smoothing)
+                {
+                    content.transform.position = Vector3.MoveTowards(content.transform.position, hitPose.position, Time.deltaTime * moveSpeed);
+                }
+                else
+                {
+                    m_SessionOrigin.MakeContentAppearAt(content, hitPose.position, m_Rotation);
+                }
+                /////////////////////////////////////////////////////////////////
+            }
+        }
+
+        static List<ARRaycastHit> s_Hits = new List<ARRaycastHit>();
+
+        ARSessionOrigin m_SessionOrigin;
+
+        ARRaycastManager m_RaycastManager;
+    }
+}
---
+++ PHW2/Assets/PHW2/ScaleController.cs
@@ -0,0 +1,109 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.XR.ARFoundation;
+
+namespace UnityEngine.XR.ARFoundation.Samples
+{
+    /// <summary>
+    /// Sets the scale of the ARSessionOrigin according to the value of a UI.Slider.
+    /// </summary>
+    [RequireComponent(typeof(PHW2_main))]
+    public class ScaleController : MonoBehaviour
+    {
+        PHW2_main m_PHW2_main;
+
+        [SerializeField]
+        [Tooltip("The slider used to control the scale factor.")]
+        Slider m_Slider;
+
+        /// <summary>
+        /// The slider used to control the scale factor.
+        /// </summary>
+        public Slider slider
+        {
+            get { return m_Slider; }
+            set { m_Slider = value; }
+        }
+
+        [SerializeField]
+        [Tooltip("The text used to display the current scale factor on the screen.")]
+        Text m_Text;
+
+        /// <summary>
+        /// The text used to display the current scale factor on the screen.
+        /// </summary>
+        public Text text
+        {
+            get { return m_Text; }
+            set { m_Text = value; }
+        }
+
+        [SerializeField]
+        [Tooltip("Minimum scale factor.")]
+        public float m_Min = .1f;
+
+        /// <summary>
+        /// Minimum scale factor.
+        /// </summary>
+        public float min
+        {
+            get { return m_Min; }
+            set { m_Min = value; }
+        }
+
+        [SerializeField]
+        [Tooltip("Maximum scale factor.")]
+        public float m_Max = 10f;
+
+        /// <summary>
+        /// Maximum scale factor.
+        /// </summary>
+        public float max
+        {
+            get { return m_Max; }
+            set { m_Max = value; }
+        }
+
+        /// <summary>
+        /// Invoked whenever the slider's value changes
+        /// </summary>
+        public void OnSliderValueChanged()
+        {
+            if (slider != null)
+                scale = slider.value * (max - min) + min;
+        }
+
+        //////////////////////////// TO DO #1 ///////////////////////////
+        float scale
+        {
+            get
+            {
+                return m_PHW2_main.scale;
+            }
+            set
+            {
+                m_PHW2_main.scale = value;
+                UpdateText();
+            }
+        }
+        ////////////////////////////////////////////////////////////////
+
+        void Awake()
+        {
+            m_PHW2_main = GetComponent<PHW2_main>();
+        }
+
+        void OnEnable()
+        {
+            if (slider != null)
+                slider.value = (scale - min) / (max - min);
+            UpdateText();
+        }
+
+        void UpdateText()
+        {
+            if (text != null)
+                text.text = "Scale: " + scale;
+        }
+    }
+}
